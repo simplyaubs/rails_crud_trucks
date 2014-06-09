0da4077f55934b8a3c55f9917aeb35eca24f12d5
Aubrey Howell <simplyaubs@gmail.com>
2014-06-08 23:17:05 -0600

    User can update a truck.

--- app/controllers/trucks_controller.rb
+++ app/controllers/trucks_controller.rb
@@ -15,6 +15,21 @@ class TrucksController < ApplicationController
     end
   end
 
+  def show
+    @truck = Truck.find(params[:id])
+  end
+
+  def edit
+    @truck = Truck.find(params[:id])
+  end
+
+  def update
+    @truck = Truck.find(params[:id])
+    @truck.update_attributes!(truck_params)
+
+    redirect_to trucks_path
+  end
+
   private
   def truck_params
     params.require(:truck).permit(:make, :model)
--- spec/features/trucks_spec.rb
+++ spec/features/trucks_spec.rb
@@ -11,4 +11,26 @@ feature 'CRUD favorite trucks' do
     expect(page).to have_content 'Ford'
     expect(page).to have_content 'Ranger'
   end
+
+  scenario 'User can update a truck' do
+    visit '/'
+    expect(page).to have_content 'Welcome'
+    click_on 'Add a truck'
+    fill_in 'Make', with: 'Ford'
+    fill_in 'Model', with: 'Ranger'
+    click_on 'Add truck'
+    expect(page).to have_content 'Ford'
+    expect(page).to have_content 'Ranger'
+    click_on 'Ford'
+    expect(page).to have_content 'Ford'
+    expect(page).to have_content 'Ranger'
+    click_on 'Edit'
+    fill_in 'Make', with: 'Toyota'
+    fill_in 'Model', with: 'Tacoma'
+    click_on 'Update truck'
+    expect(page).to have_content 'Toyota'
+    expect(page).to have_content 'Tacoma'
+    expect(page).to_not have_content 'Ford'
+    expect(page).to_not have_content 'Ranger'
+  end
 end
